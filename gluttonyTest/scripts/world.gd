d7b520c42ef8f4c25dfa846568792ae7c351a11c
Etukurud <etukurudinesh@gmail.com>
2024-03-22 16:01:18 +0000

    highlight, add and remove tiles in testing

--- gluttonyTest/scripts/world.gd
+++ gluttonyTest/scripts/world.gd
@@ -3,22 +3,27 @@ extends TileMap
 var mouseposition : Vector2
 var prev : Vector2 = Vector2(0,0)
 var tile_cord : Vector2 = Vector2(0,0)
+@onready var toggle = false
 
 func _process(delta):
+	if Input.is_action_pressed("toggle_delete"):
+		toggle = !toggle
+	tile_placement()
+
+func highlight_tiles():
+	for i in range(-100,100):
+		for j in range(-100,100):
+			erase_cell(0, Vector2(i,j))
 	
-	#for i in range(0,3):
-	#	for j in range(0,3):
-	#		set_cell(0, Vector2(i,j), 4, Vector2(1,2),0)
-	set_cell(0, Vector2(0,1), 4, Vector2(1,2), 0)
-	set_cell(0, Vector2(0,2), 4, Vector2(0,2), 0)
-	set_cell(0, Vector2(1,2), 4, Vector2(1,2), 0)
-	set_cell(0, Vector2(0,3), 4, Vector2(1,2), 0)
-	set_cell(0, Vector2(0,4), 4, Vector2(1,2), 0)
+	mouseposition = get_local_mouse_position()
+	tile_cord = local_to_map(mouseposition)
+	set_cell(0, tile_cord, 4, Vector2(1,2), 0)
 
 func tile_placement() -> void:
 	if tile_cord != prev:
 		prev = tile_cord
-		erase_cell(0, prev)
+		if toggle:
+			erase_cell(0, prev)
 		print("tile coordinates : ", tile_cord, ",  mouse position : ", mouseposition)
 	#if Input.is_action_pressed("LeftClick"):
 	mouseposition = get_local_mouse_position()
@@ -27,3 +32,5 @@ func tile_placement() -> void:
 		set_cell(0, tile_cord, 4, Vector2(1,2), 0)
 	if Input.is_action_pressed("RightClick"):
 		erase_cell(0, tile_cord)
+
+
