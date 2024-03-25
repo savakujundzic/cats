383eca08f42b1bea6a936085b90cd71488ce06df
savakujundzic <kujundzicsava4@gmail.com>
2024-03-25 20:32:34 +0100

    zoom

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -14,3 +14,10 @@ func _input(event):
 		var mouseposition = mouse_pos - event.position
 		translate(mouseposition)
 		mouse_pos = event.position
+func _physics_process(delta):
+	if Input.is_action_just_released('scrolldown'):
+		zoom.x = max(1.0, zoom.x - 0.05)
+		zoom.y = max(1.0, zoom.y - 0.05)
+	if Input.is_action_just_released('scrollup'):
+		zoom.x = min(3.0, zoom.x + 0.05)
+		zoom.y = min(3.0, zoom.y + 0.05)
