2e1a9d289153f0686724c721e0b9b921e0d55375
Etukurud <etukurudinesh@gmail.com>
2024-03-25 00:12:19 +0000

    feat: add camera zoom in and out

--- gluttonyTest/scripts/camera.gd
+++ gluttonyTest/scripts/camera.gd
@@ -14,3 +14,11 @@ func _input(event):
 	if event is InputEventMouseMotion and touchEnabled:
 		translate(mouse_pos - event.global_position)
 		mouse_pos = event.global_position
+
+func _physics_process(delta):
+	if Input.is_action_just_released('scrolldown'):
+		zoom.x = max(1.0, zoom.x - 0.05)
+		zoom.y = max(1.0, zoom.y - 0.05)
+	if Input.is_action_just_released('scrollup'):
+		zoom.x = min(3.0, zoom.x + 0.05)
+		zoom.y = min(3.0, zoom.y + 0.05)
